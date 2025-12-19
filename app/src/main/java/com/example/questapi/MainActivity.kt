909003f794d7367d27a6159a68f12ef67b33e706
Mihaly24 <fairuzk20@gmail.com>
2025-12-19 17:19:02 +0700

    Update MainActivity.kt untuk menajalankan aplikasi

--- app/src/main/java/com/example/questapi/MainActivity.kt
+++ app/src/main/java/com/example/questapi/MainActivity.kt
@@ -20,28 +20,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             QuestAPITheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DataSiswaApp(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuestAPITheme {
-        Greeting("Android")
     }
 }
