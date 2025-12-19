feeb973bab4fd03794f4472e46fbba4822b36c03
Mihaly24 <fairuzk20@gmail.com>
2025-12-19 16:32:27 +0700

    Menambahkan file da isi ServiceApiSiswa.kt

---
+++ app/src/main/java/com/example/questapi/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.questapi.apiservice
+
+import com.example.questapi.modeldata.DataSiswa
+import okhttp3.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa {
+
+    @GET("bacaTeman.php")
+    suspend fun getSiswa(): List<DataSiswa>
+
+    @POST("insertTM.php")
+    suspend fun postSiswa(
+        @Body dataSiswa: DataSiswa):retrofit2.Response<Void>
+}
