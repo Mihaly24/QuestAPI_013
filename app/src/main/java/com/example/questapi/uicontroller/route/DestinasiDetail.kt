d8d14058a2e732d5571c9d75fe413014dc2eeeb1
Mihaly24 <fairuzk20@gmail.com>
2025-12-26 18:39:47 +0700

    Update DestinasiDetail.kt

--- app/src/main/java/com/example/questapi/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi/uicontroller/route/DestinasiDetail.kt
@@ -2,7 +2,9 @@ package com.example.questapi.uicontroller.route
 
 import com.example.questapi.R
 
-object DestinasiDetail : DestinasiNavigasi {
-    override val route = "item_detail"
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
     override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
 }
