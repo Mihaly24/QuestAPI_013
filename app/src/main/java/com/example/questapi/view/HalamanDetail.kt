4c90d624e72043dc3ba74a249fb342f89d1b21bd
Mihaly24 <fairuzk20@gmail.com>
2025-12-26 18:44:01 +0700

    Membuat fungsi detailsiswascreen

--- app/src/main/java/com/example/questapi/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi/view/HalamanDetail.kt
@@ -1,2 +1,15 @@
 package com.example.questapi.view
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi.viewmodel.DetailViewModel
+import com.example.questapi.viewmodel.provider.PenyediaViewModel
+
+@Composable
+fun DetailSiswaScreen(
+    navigateToEditItem: (Int) -> Unit,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
